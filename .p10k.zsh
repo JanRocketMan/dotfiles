1650fda561a0f45c717a13bc925af2ed3f2a6a08
JanRocketMan <cherv277@gmail.com>
2026-04-07 18:54:58 +0400

    p10k: use truncate_absolute for consistent 1-char path shortening

--- .p10k.zsh
+++ .p10k.zsh
@@ -40,7 +40,7 @@
   # ── dir (current directory) ──────────────────────────────────────────────
 
   typeset -g POWERLEVEL9K_DIR_FOREGROUND=green
-  typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=truncate_to_unique
+  typeset -g POWERLEVEL9K_SHORTEN_STRATEGY=truncate_absolute
   typeset -g POWERLEVEL9K_SHORTEN_DIR_LENGTH=1
   typeset -g POWERLEVEL9K_SHORTEN_DELIMITER=
   typeset -g POWERLEVEL9K_DIR_ANCHOR_BOLD=false
